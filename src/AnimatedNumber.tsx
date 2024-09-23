2309200aa8e7e4209a87abedfa6a82bd0d434d52
George Wizeman <moxorama@yandex.ru>
2024-09-23 17:53:01 +0700

    fix: fixed hook deps in more explicit way

--- src/AnimatedNumber.tsx
+++ src/AnimatedNumber.tsx
@@ -1,8 +1,9 @@
-import { useMemo, useCallback } from "react";
-import { View, TextStyle } from "react-native";
+import { useMemo, useCallback } from 'react';
+import { View } from 'react-native';
+import type { TextStyle } from 'react-native';
 
-import { Reel } from "./Reel";
-import { styles, lineHeightMultiplier } from "./AnimatedNumberStyle";
+import { Reel } from './Reel';
+import { styles, lineHeightMultiplier } from './AnimatedNumberStyle';
 
 type AnimatedNumberProps = {
   value: number;
@@ -15,8 +16,14 @@ type AnimatedNumberProps = {
 function AnimatedNumber(props: AnimatedNumberProps) {
   const { value, format, fontSize, textStyle, duration } = props;
 
-  const formattedValue = useMemo(() => { return format ? format.format(value) : value.toString() }, [value, format]);
-  const symbols = useMemo(() => formattedValue.toString().split(""), [formattedValue]);
+  const formattedValue = useMemo(() => {
+    return format ? format.format(value) : value.toString();
+  }, [value, format]);
+
+  const symbols = useMemo(
+    () => formattedValue.toString().split(''),
+    [formattedValue]
+  );
 
   const renderReels = useCallback(() => {
     return symbols.map((symbol, index) => {
@@ -30,7 +37,7 @@ function AnimatedNumber(props: AnimatedNumberProps) {
         />
       );
     });
-  }, [value, fontSize, duration]);
+  }, [symbols, fontSize, duration, textStyle]);
 
   const contentContainerStyle = useMemo(
     () => ({
